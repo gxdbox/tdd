8839220c5ede2fbd831792b0f5d9362ae246bd2e
gxdbox <gxd683@126.com>
2022-06-20 16:17:57 +0800

    lyk1

---
+++ src/main/java/com/example/tdd/controller/Lyk1.java
@@ -0,0 +1,12 @@
+package com.example.tdd.controller;
+
+/**
+ * @author gxd18
+ * @date 2022/6/20
+ * @description
+ */
+public class Lyk1 {
+    public static void main(String[] args) {
+        System.out.println("lyk1");
+    }
+}
