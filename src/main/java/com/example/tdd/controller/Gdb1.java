dc122f85b113a25de5e3f8bca3329bbb93d0b74f
gxdbox <gxd683@126.com>
2022-06-20 16:35:24 +0800

    git test

---
+++ src/main/java/com/example/tdd/controller/Gdb1.java
@@ -0,0 +1,12 @@
+package com.example.tdd.controller;
+
+/**
+ * @author gxd18
+ * @date 2022/6/20
+ * @description
+ */
+public class Gdb1 {
+    public static void main(String[] args) {
+        System.out.println("gdb1");
+    }
+}
